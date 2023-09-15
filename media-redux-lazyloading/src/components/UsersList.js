b28a7defd4349972f114e7b47ff47bcf779cf0dd
Doven Obertinca <doven.obertinca@gmail.com>
2023-09-15 14:44:09 +0200

    better skeleton display

--- media-redux-lazyloading/src/components/UsersList.js
+++ media-redux-lazyloading/src/components/UsersList.js
@@ -1,24 +1,10 @@
-import { useEffect, useState, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import { addUser } from "../store";
-
-const useThunk = (thunk) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const dispatch = useDispatch();
-
-  const runThunk = useCallback(() => {
-    setIsLoading(true);
-    dispatch(thunk())
-      .unwrap()
-      .catch((err) => setError(err))
-      .finally(() => setIsLoading(false));
-  }, [dispatch, thunk]);
-  return [runThunk, isLoading, error];
-};
+import { useThunk } from "../hooks/use-thunk";
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -57,11 +43,12 @@ const UsersList = () => {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUsers ? (
-          "Creating User"
-        ) : (
-          <Button onClick={handleUserAdd}> + Add User</Button>
-        )}
+
+        <Button loading={isCreatingUsers} onClick={handleUserAdd}>
+          {" "}
+          + Add User
+        </Button>
+
         {creatingUserError && "Error creating user"}
       </div>
 
