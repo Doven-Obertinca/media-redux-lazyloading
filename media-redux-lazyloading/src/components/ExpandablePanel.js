6d399bab2f740fc08eca588c01764e83b1b10bc2
Doven Obertinca <doven.obertinca@gmail.com>
2023-09-20 14:44:02 +0200

    addind expand

---
+++ media-redux-lazyloading/src/components/ExpandablePanel.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const ExpandablePanel = ({ header, children }) => {
+  const [expanded, isExpanded] = useState(false);
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
+      </div>
+      <div className="p-2 border-t">{children}</div>
+    </div>
+  );
+};
+
+export default ExpandablePanel;
