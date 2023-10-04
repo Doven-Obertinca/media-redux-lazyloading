abb8a8582e7ab026a30c5fbc8260c8f027c8b6f6
Doven Obertinca <doven.obertinca@gmail.com>
2023-10-04 11:11:23 +0200

    adding mutation data

--- media-redux-lazyloading/src/components/AlbumList.js
+++ media-redux-lazyloading/src/components/AlbumList.js
@@ -1,10 +1,41 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 const AlbumList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-  console.log(data, error, isLoading);
-  return <div>AlbumList {user.name}</div>;
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+  console.log("TEST", data, error, isLoading);
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        AlbumList {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album </Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default AlbumList;
